f3dcb9eb3f2d12bf8ce396bf0512e9569c3e6f00
Gustavo Santos <gustavo.heitor@outlook.com>
2022-02-23 17:14:20 -0300

    Category

--- src/main/java/com/sc/resources/CategoryResource.java
+++ src/main/java/com/sc/resources/CategoryResource.java
@@ -1,16 +1,29 @@
 package com.sc.resources;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sc.domain.Category;
 
 @RestController
 @RequestMapping (value = "/categories")
 public class CategoryResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listCategories() {
-		return "Rest is working!";
+	public List<Category> listCategories() {
+		
+		Category cat1 = new Category(1,"Computers");
+		Category cat2 = new Category(2,"Office");
+		
+		List<Category> list = new ArrayList<>();
+		list.addAll(Arrays.asList(cat1,cat2));
+		
+		return list;
 	}
 	
 }

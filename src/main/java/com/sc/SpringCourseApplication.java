8e9a9e0b2b9eaa714840599ecfc7825f76d0cbbc
Gustavo Santos <gustavo.heitor@outlook.com>
2022-02-24 09:54:57 -0300

    Product and ProductRepository

--- src/main/java/com/sc/SpringCourseApplication.java
+++ src/main/java/com/sc/SpringCourseApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.sc.domain.Category;
+import com.sc.domain.Product;
 import com.sc.repositories.CategoryRepository;
+import com.sc.repositories.ProductRepository;
 
 @SpringBootApplication
 public class SpringCourseApplication implements CommandLineRunner{
 
 	@Autowired
 	CategoryRepository categoryRep;
+	
+	@Autowired
+	ProductRepository productRep;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCourseApplication.class, args);
@@ -26,7 +31,18 @@ public class SpringCourseApplication implements CommandLineRunner{
 		Category cat1 = new Category(null,"computers");
 		Category cat2 = new Category(null,"office");
 		
+		Product p1 = new Product(null, "Computer", 2000.00);
+		Product p2 = new Product(null, "Printer", 800.00);
+		Product p3 = new Product(null, "Mouse", 80.00);
+		
+		cat1.getProducts().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProducts().add(p2);
+		p1.getCategories().add(cat1);
+		p2.getCategories().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategories().add(cat1);
+		
 		categoryRep.saveAll(Arrays.asList(cat1,cat2));
+		productRep.saveAll(Arrays.asList(p1,p2,p3));
 		
 	}
 
--- src/main/java/com/sc/domain/Product.java
+++ src/main/java/com/sc/domain/Product.java
@@ -9,25 +9,30 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 @Entity
-public class Category implements Serializable{
+public class Product implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
+	private Double price;
 	
-	@ManyToMany(mappedBy = "categories")
-	private List<Product> products = new ArrayList<>();
+	@ManyToMany
+	@JoinTable(name = "product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private List<Category> categories = new ArrayList<>();
 	
-	public Category() {}
+	public Product () {}
 
-	public Category(Integer id, String name) {
+	public Product(Integer id, String name, Double price) {
 		this.id = id;
 		this.name = name;
+		this.price = price;
 	}
 
 	public Integer getId() {
@@ -38,12 +43,12 @@ public class Category implements Serializable{
 		return name;
 	}
 
-	public List<Product> getProducts() {
-		return products;
+	public Double getPrice() {
+		return price;
 	}
 
-	public void setProducts(List<Product> products) {
-		this.products = products;
+	public List<Category> getCategories() {
+		return categories;
 	}
 
 	public void setId(Integer id) {
@@ -52,6 +57,14 @@ public class Category implements Serializable{
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public void setCategories(List<Category> categories) {
+		this.categories = categories;
 	}
 
 	@Override
@@ -67,7 +80,7 @@ public class Category implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		return Objects.equals(id, other.id);
 	}
 
